50e05427e94b440fbea7b8a33ebfc4b89e434c81
Andrea Barutta <andreabarutta123@gmail.com>
2023-05-05 18:25:34 +0200

    checkstyle e codice senza test

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build](https://github.com/Barutta02/MTSS_Assignment_2/actions/workflows/build.yml/badge.svg)](https://github.com/Barutta02/MTSS_Assignment_2/actions/workflows/build.yml)
 ![coverage](.github/badges/jacoco.svg)
 ![branches_coverage](.github/badges/branches.svg)
-
+![Checkstyle](https://img.shields.io/badge/style-checkstyle-green.svg)
 
 # MTSS_Assignment_2
 # Convertitore integer to roman
--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -4,14 +4,82 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class RomanPrinter {
+    public static Map<String, String> letters;
+
+    static {
+          letters   = new HashMap<String, String>();
+          letters.put("I", " _____ \n" +
+                               "|_   _|\n" +
+                               "  | |  \n"+
+                               "  | |  \n"+
+                               " _| |_ \n"+
+                               "|_____|");
+  
+          letters.put("V",  "__      __ \n" +
+                            "\\ \\    / / \n" +
+                            " \\ \\  / /  \n" +
+                            "  \\ \\/ /   \n"+
+                            "   \\  /    \n"+
+                            "    \\/     ");
+  
+          letters.put("X",  "__   __  \n" +
+                                "\\ \\ / /  \n" +
+                                " \\ V /   \n"+
+                                "  > <    \n"+
+                                " / . \\   \n"+
+                                "/_/ \\_\\  ");
+  
+  
+          letters.put("L",  " _      \n" +
+                                "| |     \n" +
+                                "| |     \n"+
+                                "| |     \n"+
+                                "| |____ \n"+
+                                "|______|");
+  
+          letters.put("C", "  _____ \n"
+                             + " / ____|\n"
+                             + "| |     \n"
+                             + "| |     \n"
+                             + "| |____ \n"
+                             + " \\_____|");
+  
+          letters.put("D", " _____   \n"
+                              +"|  __ \\  \n"
+                              +"| |  | | \n"
+                              +"| |  | | \n"
+                              +"| |__| | \n"
+                              +"|_____/  ");
+  
+  
+          letters.put("M", " __  __  \n" +
+                               "|  \\/  | \n" +
+                               "| \\  / | \n"+
+                               "| |\\/| | \n"+
+                               "| |  | | \n"+
+                               "|_|  |_| ");
+    }
+  
+  
+
     public static String print(int num) {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
 
-    private static String printAsciiArt(String romanNumber) {
-        // TODO
-        return null;
+    private static String printAsciiArt(String s) {
+        String art="";
+      for(int i = 0 ; i< 6;i++){
+          for(int j = 0; j<s.length();j++){
+              art+= letters.get(String.valueOf(s.charAt(j))).split("\\r?\\n")[i];
+          }
+          art+='\n';
+      }
+      return art;
     }
 
 }
